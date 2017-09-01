c3c7907a2cdbbf8bcd7dfbaa550a786a77f15daa
Michael <michael@sleepace.net>
2017-09-01 11:30:49 +0800

    Initial Commit

---
+++ Psychology/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Psychology
+//
+//  Created by medica_mac on 2017/9/1.
+//  Copyright © 2017年 com.medica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Psychology/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Psychology
+//
+//  Created by medica_mac on 2017/9/1.
+//  Copyright © 2017年 com.medica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
